5492c13ccc46aa4c428fe5b6db9ecc8a935cee23
adams52 <adams.b52@gmail.com>
2018-04-18 21:18:04 -0500

    Utilizes the WordUnscramble code to find actual words! Small defect: It's possible multiple permutations will return the same word. Need to change from a list to a set

--- WordUnscramble/WordUnscrambleWeb/src/main/java/org/aperture/com/controller/HomeController.java
+++ WordUnscramble/WordUnscrambleWeb/src/main/java/org/aperture/com/controller/HomeController.java
@@ -1,10 +1,10 @@
 package org.aperture.com.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.aperture.com.utility.WordsBySize;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +13,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private WordsBySize wordsBySize;
+	
 	@GetMapping("/wordUnscramble")
 	public String home(@RequestParam(name="letters") String letters, Model model) {
-		Map<String, List<String>> results = new HashMap<String, List<String>>();
-		List<String> a1 = new ArrayList<String>();
-		a1.add("a");
-		a1.add("b");
-		a1.add("c");
-		
-		List<String> a2 = new ArrayList<String>();
-		a2.add("aa");
-		a2.add("bb");
-		a2.add("cc");
-		
-		results.put("1", a1);
-		results.put("2", a2);
+		Map<String, List<String>> results = wordsBySize.getWordsBySize(letters);
+//		Map<String, List<String>> results = new HashMap<String, List<String>>();
+//		List<String> a1 = new ArrayList<String>();
+//		a1.add("a");
+//		a1.add("b");
+//		a1.add("c");
+//		
+//		List<String> a2 = new ArrayList<String>();
+//		a2.add("aa");
+//		a2.add("bb");
+//		a2.add("cc");
+//		
+//		results.put("1", a1);
+//		results.put("2", a2);
 		
 		
 		model.addAttribute("letters", letters);
