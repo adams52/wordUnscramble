9768e3d68b28adb2bb9faa47408da3a8a8914d64
adams52 <adams.b52@gmail.com>
2018-04-18 20:13:37 -0500

    Basic UI for word unscramble using thymeleaf and spring boot

---
+++ WordUnscramble/WordUnscrambleWeb/src/main/java/org/aperture/com/controller/HomeController.java
@@ -0,0 +1,37 @@
+package org.aperture.com.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/wordUnscramble")
+	public String home(@RequestParam(name="letters") String letters, Model model) {
+		Map<String, List<String>> results = new HashMap<String, List<String>>();
+		List<String> a1 = new ArrayList<String>();
+		a1.add("a");
+		a1.add("b");
+		a1.add("c");
+		
+		List<String> a2 = new ArrayList<String>();
+		a2.add("aa");
+		a2.add("bb");
+		a2.add("cc");
+		
+		results.put("1", a1);
+		results.put("2", a2);
+		
+		
+		model.addAttribute("letters", letters);
+		model.addAttribute("results", results);
+		return "results";
+	}
+}
