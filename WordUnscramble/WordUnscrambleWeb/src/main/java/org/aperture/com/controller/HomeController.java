c6260dc2e5054af6dbe2dc7458a657f0e1c81ff5
adams52 <adams.b52@gmail.com>
2018-04-23 11:58:59 -0500

    Turned Controller into Rest service instead of part of app

--- WordUnscramble/WordUnscrambleWeb/src/main/java/org/aperture/com/controller/HomeController.java
+++ WordUnscramble/WordUnscrambleWeb/src/main/java/org/aperture/com/controller/HomeController.java
@@ -3,26 +3,35 @@ package org.aperture.com.controller;
 import java.util.Map;
 import java.util.Set;
 
+import org.aperture.com.permutations.Permutations;
+import org.aperture.com.utility.MapDictionary;
 import org.aperture.com.utility.WordsBySize;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.json.JSONObject;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
-	
-	@Autowired
-	private WordsBySize wordsBySize;
 	
 	@GetMapping("/wordUnscramble")
 	public String home(@RequestParam(name="letters") String letters, Model model) {
+		WordsBySize wordsBySize = new WordsBySize();
+		MapDictionary dictionary = new MapDictionary();
+		Permutations perm = new Permutations();
+		
+		wordsBySize.setDictionary(dictionary);
+		wordsBySize.setPermutations(perm);
+		
 		Map<String, Set<String>> results = wordsBySize.getWordsBySize(letters);
 		
+		JSONObject jsonObject = new JSONObject(results);
 		
 		model.addAttribute("letters", letters);
 		model.addAttribute("results", results);
-		return "results";
+		
+		
+		return jsonObject.toString();
 	}
 }
